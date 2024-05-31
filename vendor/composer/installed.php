604133037402b721583affbae53a0097b0e83237
robert-hsw <149268290+robert-hsw@users.noreply.github.com>
2024-05-31 13:30:29 -0500

    Initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'hsw/photoupload',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '014469d72b55d08b2fe60b7cebcf666f8b89db40',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hsw/photoupload' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '014469d72b55d08b2fe60b7cebcf666f8b89db40',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
